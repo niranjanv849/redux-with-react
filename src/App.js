e8b27acadb9f78462567e188e6e29865801a8605
niranjanv849 <niranjanv849@gmail.com>
2022-05-30 10:25:01 +0530

    rootReducer ➕✅

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
 import "./App.css";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="app">
+        <HooksCakeContainer />
         <CakeContainer />
+        <IceCreamContainer />
       </div>
     </Provider>
   );
---
+++ src/components/IceCreamContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect, Connect } from "react-redux";
+import { buyIceCream } from "../redux";
+
+function IceCreamContainer(props) {
+  return (
+    <div>
+      <h2>Number of Icecream - {props.numOfIceCreams}</h2>
+      <button onClick={props.buyIceCream}>Buy Icecream</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    numOfIceCreams: state.iceCream.numOfIceCream,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyIceCream: () => dispatch(buyIceCream()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(IceCreamContainer);
